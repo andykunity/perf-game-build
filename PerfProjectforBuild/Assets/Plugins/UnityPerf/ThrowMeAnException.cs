bf44ad00f43621049f36eae3b9724770ad7a37bb
Andy K <andyk@unity3d.com>
2015-02-10 13:45:43 -0800

    initial project

---
+++ PerfProjectforBuild/Assets/Plugins/UnityPerf/ThrowMeAnException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.CrashLog;
+using System.Collections;
+
+public class ThrowMeAnException : MonoBehaviour 
+{
+
+    void Awake()
+    {
+        //Replace the "1" with your project id
+		Environment.environment = "staging";
+		CrashReporting.Init("ee0b870f-4028-4ff5-8922-9fa2b1ee7b73");
+    }
+
+	public void CrashForMe()
+	{
+		throw new Exception("Button press exception");
+	}
+
+}
