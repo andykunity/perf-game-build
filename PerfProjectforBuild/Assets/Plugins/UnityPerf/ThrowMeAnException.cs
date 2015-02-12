ee6334f03fc4515aa0aced5d422a37d92b12babf
Andy K <andyk@unity3d.com>
2015-02-12 08:59:17 -0800

    Made it for Igor's game
    Pointed towards production for “Igor’s Game”

--- PerfProjectforBuild/Assets/Plugins/UnityPerf/ThrowMeAnException.cs
+++ PerfProjectforBuild/Assets/Plugins/UnityPerf/ThrowMeAnException.cs
@@ -9,13 +9,13 @@ public class ThrowMeAnException : MonoBehaviour
     void Awake()
     {
         //Replace the "1" with your project id
-		Environment.environment = "staging";
-		CrashReporting.Init("ee0b870f-4028-4ff5-8922-9fa2b1ee7b73");
+		//Environment.environment = "staging";
+		CrashReporting.Init("b0f0ea8d-6282-42f1-9b8b-44998bb0c568");
     }
 
 	public void CrashForMe()
 	{
-		throw new Exception("Button press exception");
+		throw new Exception("Button press exception for Igor's Build");
 	}
 
 }
